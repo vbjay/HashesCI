1e8f8795e5173f118a4c59eb175653d8a9555be2
Jay Asbury <vbjay.net@gmail.com>
2021-02-05 02:25:54 -0500

    Add hashes powershell script

---
+++ Build/hashes.ps1
@@ -0,0 +1,10 @@
+﻿Set-Location $PSScriptRoot\..\HashedCI\bin\Release
+$lines = Get-ChildItem -Recurse -File  | 
+Get-FileHash -Algorithm SHA512 | ForEach-Object { 
+    $pth = Resolve-Path -Relative $_.Path
+    $unixPath = $pth -replace '\\', '/'
+    $hash = $_.Hash.ToUpper()
+    "$unixPath`t$hash" }
+
+$lines -join "`r`n" >  hashes.txt 
+
