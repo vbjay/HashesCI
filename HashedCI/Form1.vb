4abe9674ad9e18be53e564198df170d58b9a3e01
Jay Asbury <vbjay.net@gmail.com>
2021-02-06 13:50:17 -0500

    chains

--- HashedCI/Form1.vb
+++ HashedCI/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Imports UnitsNet
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show($"A meter is {Length.FromMeters(1).LightYears} light years.{vbCrLf}{ThisAssembly.Git.Sha}")
+        MessageBox.Show($"A meter is {Length.FromMeters(1).Chains} chains.{vbCrLf}{ThisAssembly.Git.Sha}")
 
     End Sub
 End Class
