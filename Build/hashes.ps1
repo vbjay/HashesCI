6044c6ca5e866ac99e52cdfd7b8a9545bff6b075
Jay Asbury <vbjay.net@gmail.com>
2021-02-06 14:11:45 -0500

    WIP add hash file based on commit

--- Build/hashes.ps1
+++ Build/hashes.ps1
@@ -1,10 +1,30 @@
-﻿Set-Location $PSScriptRoot\..\HashedCI\bin\Release
-$lines = Get-ChildItem -Recurse -File  | 
-Get-FileHash -Algorithm SHA512 | ForEach-Object { 
-    $pth = Resolve-Path -Relative $_.Path
-    $unixPath = $pth -replace '\\', '/'
-    $hash = $_.Hash.ToUpper()
-    "$unixPath`t$hash" }
+﻿Set-Location $PSScriptRoot\..\
+    
+$hsh = git rev-parse master
+$fl = "$PSScriptRoot\$hsh.txt" 
+ git checkout hashes
+if (!(Test-Path "$fl")) {
+    $lines = Get-ChildItem -Recurse -File -Path "$PSScriptRoot\..\HashedCI\bin\Release"  | 
+    Get-FileHash -Algorithm SHA512 | ForEach-Object { 
+        $pth = Resolve-Path -Relative $_.Path
+        $unixPath = $pth -replace '\\', '/'
+        $hash = $_.Hash.ToUpper()
+        "$unixPath`t$hash" }
+    
+    $lines -join "`r`n" > "$fl"
 
-$lines -join "`r`n" >  hashes.txt 
+   
+    git add "$fl"
+    $msg = "$hsh hashes"
+    
+    git commit -m "$msg"
+    git branch -v
+        
+}
+else {
+    "file exists"
+
+}
+
+
 
